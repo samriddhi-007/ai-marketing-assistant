baade4eb2666ec2c2f38a82352ea179a139f755f
samriddhi--007 <samriddhi1157@gmail.com>
2026-05-20 19:35:42 +0530

    Fix indentation

--- app.py
+++ app.py
@@ -38,6 +38,6 @@ def chat():
         return jsonify({
             "reply": str(e)
         })
-    if __name__ == "__main__":
+        if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
